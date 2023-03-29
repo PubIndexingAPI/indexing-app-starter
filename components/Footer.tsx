0210cad89708bf5661b8fa83b417e93ddfb0feb6
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-03-29 21:03:59 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
         <footer>
             <p>
                 <a 
-                href="https://github.com/PubIndexingAPI" target="_blank">Github</a> - <a 
-                href="https://docs.pubindexapi.com/" target="_blank">Docs</a> - <a 
-                href="https://pubindexapi.instatus.com/" target="_blank">API Service Status</a>
+                    href="https://github.com/PubIndexingAPI/indexing-app-starter" target="_blank">Github</a> - <a 
+                    href="https://docs.pubindexapi.com/" target="_blank">Docs</a> - <a 
+                    href="https://pubindexapi.instatus.com/" target="_blank">API Service Status</a>
             </p>
         </footer>
     );
